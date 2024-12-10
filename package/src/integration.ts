04aed58d15ba876ee1d3d1892c73c40ba79e1993
ginger <26461046+gingerchew@users.noreply.github.com>
2024-12-10 15:46:04 -0500

    Initial implementation of astro-globals integration

--- package/src/integration.ts
+++ package/src/integration.ts
@@ -1,10 +1,32 @@
-import { defineIntegration } from "astro-integration-kit";
+import { addVirtualImports, defineIntegration } from "astro-integration-kit";
+import { z } from "astro/zod";
 
 export const integration = defineIntegration({
-	name: "package-name",
-	setup() {
+	name: "astro-globals",
+	optionsSchema: z.object({
+		sources: z.array(z.string())
+	}),
+	setup({ name, options }) {
+		
+		const exports = options.sources.map(file => `export * from "${file}"`).join('\n');
+
 		return {
-			hooks: {},
+			hooks: {
+				'astro:config:setup': async (params) => {
+					addVirtualImports(params, {
+						name, 
+						imports: {
+							['globals:data']: exports
+						}
+					})
+				},
+				'astro:config:done': async ({ injectTypes }) => {
+					injectTypes({
+						filename: 'types.d.ts',
+						content: 'declare module "globals:data";'
+					})
+				}
+			},
 		};
 	},
 });
